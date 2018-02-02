c0c020806a2019f30981519329408d9e24596d8f
NikkesR <35994941+NikkesR@users.noreply.github.com>
2018-02-02 14:25:36 +0530

    nikkes7.C

---
+++ nikkes7.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int a,i;
+	printf("Enter the number");
+	scanf("%d,&a);
+	for(i=1;i<=a;i++)
+  {
+	printf("Hello");
+  }
+  getch();
+	return 0;
+}
