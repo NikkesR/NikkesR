55d575649fa7f755cd84ce77b673b05194335c9a
NikkesR <35994941+NikkesR@users.noreply.github.com>
2018-02-02 13:43:50 +0530

    nikkes3.C

---
+++ nikkes3.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char b;
+clrscr();
+printf("enter the alphabet");
+scan("%c",&b);
+if(b=='a'!!b=='e'!!b=='i'!!b=='o'!!b=='u')
+printf("vowel");
+else
+printf("consonant");
+getch();
+return 0;
+}
