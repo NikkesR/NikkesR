cbdb9f2b80a75696875499585b382bdf54b2b0bb
NikkesR <35994941+NikkesR@users.noreply.github.com>
2018-02-19 16:22:35 +0530

    program24-19.C

---
+++ program24-19.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b[100],i,j,n;
+	printf("Enter the digits:");
+	scanf("%d",&n);
+	printf("Array elements ");
+	for(i=0;i<n;i++)
+{
+scanf("%d",&b[i]);
+ 
+}
+  for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+{
+	if(b[i]>b[j])
+{
+	a=b[j];
+	b[j]=b[i];
+	b[i]=a;
+}
+}
+}
+for(i=0;i<n;i++)
+printf("%d",b[i]);
+getch();
+return 0;
+}
