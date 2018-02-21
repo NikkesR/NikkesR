03f915edbb373b52697bff29bf6eea183cf938e6
NikkesR <35994941+NikkesR@users.noreply.github.com>
2018-02-21 15:58:08 +0530

    program26-21.C

---
+++ program26-21.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int i,a,b[100],n,temp,j;
+ printf("enter the number");
+ scanf("%d",&n);
+ for(i=0;i<=n;i++)
+ {
+ scanf("%d",&b[i]);
+ }
+ for(a=i+1;a<n;a++)
+ {
+ scanf("%d",&b[a]);
+ }
+ if(b[i]>b[j])
+ printf("%d",&b[a]);
+ getch();
+ return 0;
+}
