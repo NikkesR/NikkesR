2ba9429a9602cde3b24770d09a4133e5821dba3c
NikkesR <35994941+NikkesR@users.noreply.github.com>
2018-03-07 15:54:45 +0530

    program51-07.C

---
+++ program51-07.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+ {
+ int a,b,c,d;
+ clrscr();
+ printf("Enter the three digit number: ");
+ scanf("%d",&a);
+ b=a/100;
+ scanf("%d",&b);
+ c=(a-(b*100))/10;
+ scanf("%d",&c);
+ d=((a-(b*100))-(c*10));
+ scanf("%d",&d);
+ printf("%d %d %d",b,c,d);
+ getch();
+ return 0;
+ }
